1e540991136e5e597377cfd891a0f43b54db10e1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-03-26 23:59:22 +0900

    refactored: scope :effective_on ...

--- app/models/duration_limited.rb
+++ app/models/duration_limited.rb
@@ -5,12 +5,16 @@ module DurationLimited
   
   included do
     attr_accessor :current_date
+    
+    scope :effective_on,
+      ->(date) do
+        where("started_on <= ? AND (ended_on >= ? OR ended_on IS NULL)", date, date)
+      end
   end
 
   module ClassMethods
     def find(code, date)
-      record = self.where(code: code).
-        where("started_on <= ? AND (ended_on >= ? OR ended_on IS NULL)", date, date).first
+      record = self.where(code: code).effective_on(date).first
       raise ActiveRecord::RecordNotFound unless record
       record.current_date = date
       record
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,7 @@ class Product < ActiveRecord::Base
   include DurationLimited
   
   def department
-    record = Department.where(code: department_code).
-      where("started_on <= ? AND (ended_on >= ? OR ended_on IS NULL)", current_date, current_date).first
+    record = Department.where(code: department_code).effective_on(current_date).first
     raise ActiveRecord::RecordNotFound unless record
     record.current_date = current_date
     record
