c8cde3194b342ae29c6be74f5c42d3b103026383
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-03-27 16:59:41 +0900

    Department#products

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,7 @@
 class Department < ActiveRecord::Base
   include DurationLimited
+  
+  def products
+    Product.where(department_code: code).effective_on(current_date)
+  end
 end
--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -9,15 +9,42 @@ describe Department do
   let(:department1) { FactoryGirl.create(:department,
       code: "robot", name: "Department1",
       started_on: Date.new(2002, 1, 1), ended_on: nil) }
+  let(:department2) { FactoryGirl.create(:department,
+      code: "ship", name: "Department1",
+      started_on: Date.new(2002, 1, 1), ended_on: nil) }
   
-  it "部門(department)を複合キーで検索できる" do
+  before do
     department0
     department1
-    
+    department2
+  end
+  
+  it "部門(department)を複合キーで検索できる" do
     dep0 = Department.find("robot", Date.new(2001, 1, 1))
     dep0.name.should == department0.name
     
     dep1 = Department.find("robot", Date.new(2003, 1, 1))
     dep1.name.should == department1.name
+  end
+  
+  it "2001年1月1日当時の製品(product)リストを取得できる" do
+    FactoryGirl.create(:product, name: "p0", department_code: "robot",
+      started_on: Date.new(1999, 1, 1), ended_on: Date.new(2000, 8, 1))
+    FactoryGirl.create(:product, name: "p1", department_code: "robot",
+      started_on: Date.new(2000, 1, 1), ended_on: Date.new(2002, 12, 1))
+    FactoryGirl.create(:product, name: "p2", department_code: "robot",
+      started_on: Date.new(2001, 1, 1), ended_on: nil)
+    FactoryGirl.create(:product, name: "p3", department_code: "robot",
+      started_on: Date.new(2002, 1, 1), ended_on: nil)
+    FactoryGirl.create(:product, name: "p4", department_code: "ship",
+      started_on: Date.new(2000, 1, 1), ended_on: nil)
+    
+    dep = Department.find("robot", Date.new(2001, 1, 1))
+    dep.should have(2).products
+    
+    products = dep.products.order("products.started_on")
+    
+    products[0].name.should == "p1"
+    products[1].name.should == "p2"
   end
 end
