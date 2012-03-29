a8bf3731228eaabf7bcaf6e6e56960dcc7692574
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-03-29 12:14:59 +0900

    DurationLimited: belongs_to, has_many

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,5 @@
 class Category < ActiveRecord::Base
   include DurationLimited
   
-  def products
-    @product_codes ||= CategoryProductLink.where(category_code: code).
-      select(:product_code).map(&:product_code)
-    Product.where(code: @product_codes)
-  end
+  has_many :products, through: :category_product_links
 end
--- app/models/duration_limited.rb
+++ app/models/duration_limited.rb
@@ -10,6 +10,32 @@ module DurationLimited
   end
 
   module ClassMethods
+    def belongs_to(name)
+      module_eval <<-EOS, __FILE__, __LINE__ + 1
+        def #{name}
+          #{name.to_s.camelize}.where(code: #{name}_code).first
+        end
+      EOS
+    end
+    
+    def has_many(name, options = {})
+      if options[:through]
+        module_eval <<-EOS, __FILE__, __LINE__ + 1
+          def #{name}
+            @#{name.to_s.singularize}_codes ||= #{options[:through].to_s.singularize.camelize}.where(category_code: code).
+              select(:#{name.to_s.singularize}_code).map(&:#{name.to_s.singularize}_code)
+            #{name.to_s.singularize.camelize}.where(code: @#{name.to_s.singularize}_codes)
+          end
+        EOS
+      else
+        module_eval <<-EOS, __FILE__, __LINE__ + 1
+          def #{name}
+            #{name.to_s.singularize.camelize}.where(#{self.name.underscore}_code: code)
+          end
+        EOS
+      end
+    end
+    
     def find(code)
       record = self.where(code: code).first
       raise ActiveRecord::RecordNotFound unless record
